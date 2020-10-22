4009bd8fcde66a0b32482eb7f6c057fdd6ed44bf
Gurpreet Singh <preet.gsb3@gmail.com>
2020-10-23 01:13:12 +0530

    Initial project setup

---
+++ DGSelectionPro/DGSelectionPro.h
@@ -0,0 +1,18 @@
+//
+//  DGSelectionPro.h
+//  DGSelectionPro
+//
+//  Created by Gurpreet Singh on 23/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DGSelectionPro.
+FOUNDATION_EXPORT double DGSelectionProVersionNumber;
+
+//! Project version string for DGSelectionPro.
+FOUNDATION_EXPORT const unsigned char DGSelectionProVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DGSelectionPro/PublicHeader.h>
+
+
