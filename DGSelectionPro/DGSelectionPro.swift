2d121211298fffa76633b9a85b3ba9d3cfdf5526
Gurpreet Singh <preet.gsb3@gmail.com>
2020-10-23 01:31:00 +0530

    Added test cases

---
+++ DGSelectionPro/DGSelectionPro.swift
@@ -0,0 +1,22 @@
+//
+//  DGSelectionPro.swift
+//  DGSelectionPro
+//
+//  Created by Gurpreet Singh on 23/10/20.
+//
+
+import Foundation
+
+public final class DGSelectionPro {
+
+    let name = "DGSelectionPro"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- DGSelectionProTests/DGSelectionProTests.swift
+++ DGSelectionProTests/DGSelectionProTests.swift
@@ -9,25 +9,41 @@ import XCTest
 @testable import DGSelectionPro
 
 class DGSelectionProTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    var objDGSelection: DGSelectionPro!
+    
+    override func setUp() {
+        objDGSelection = DGSelectionPro()
+    }
+    
+    func testAdd() {
+        XCTAssertEqual(objDGSelection.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(objDGSelection.sub(a: 2, b: 1), 1)
+    }
+    
+    
+    
 }
